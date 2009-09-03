c0cc89d86f3d1c157616a55ba1eeb5f3b7c4214b
akrasnov <akrasnov@Antares.bq.local>
2009-09-03 09:19:33 -0430

    tooltips was fixed, unique index was added

--- machines/models.py
+++ machines/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Machine(models.Model):
-    address = models.IPAddressField('Host Address')
-    name = models.CharField('Host Name', max_length=200)
+    address = models.IPAddressField('Host Address', unique=True)
+    name = models.CharField('Host Name', max_length=200, unique=True)
     os_name = models.CharField('OS', max_length=200)
     location = models.CharField('Location', max_length=1024, null=True, blank=True)
     user = models.ForeignKey(User, null=True, blank=True)
